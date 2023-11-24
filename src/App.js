5685d3a5d9a0162dab03bde197ecf6387977745e
Zakroev Andrey <Dr.zakroev@yandex.ru>
2023-11-24 11:07:12 +0300

    add changing url during search

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import { useState, useEffect } from "react";
-
-import "./App.css";
+import React, { useState, useEffect } from "react";
 import CardList from "./components/cards-list/card-list.component";
 import SearchBox from "./components/search-box/search-box.component";
 
+import "./App.css";
+
 const App = () => {
-  const [searchField, setSearchField] = useState("");
+  const searchParams = new URLSearchParams(window.location.search);
   const [monsters, setMonsters] = useState([]);
-  const [filterMonsters, setFilterMonsters] = useState(monsters);
+  const [searchField, setSearchField] = useState(searchParams.get("q") || "");
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
@@ -16,23 +16,25 @@ const App = () => {
   }, []);
 
   useEffect(() => {
-    const newFilterMonsters = monsters.filter((monster) => {
-      return monster.name.toLowerCase().includes(searchField);
-    });
-
-    setFilterMonsters(newFilterMonsters);
-  }, [monsters, searchField]);
+    const searchParams = new URLSearchParams();
+    searchParams.set("q", searchField);
+    window.history.replaceState({}, "", `?${searchParams.toString()}`);
+  }, [searchField]);
 
   const onSearchChange = (event) => {
     const searchFieldString = event.target.value.toLowerCase();
     setSearchField(searchFieldString);
   };
 
+  const filterMonsters = monsters.filter((monster) =>
+    monster.name.toLowerCase().includes(searchField)
+  );
+
   return (
     <div className="App">
       <h1 className="app-title">Monsters Rolodex</h1>
-
       <SearchBox
+        searchField={searchField}
         onChangeHandler={onSearchChange}
         placeholder={"Искать монстра"}
         className="monsters-search-box"
