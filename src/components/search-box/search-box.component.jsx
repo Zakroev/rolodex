b5d38a180fa41cf25b757f07b2483eff40cb3748
Zakroev Andrey <Dr.zakroev@yandex.ru>
2023-11-24 11:36:43 +0300

    fix problem with input

--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -1,11 +1,17 @@
 import "./search-box.styles.css";
 
-const SearchBox = ({ className, placeholder, onChangeHandler }) => (
+const SearchBox = ({
+  className,
+  placeholder,
+  onChangeHandler,
+  searchField,
+}) => (
   <input
     className={`search-box ${className}`}
     type="search"
     placeholder={placeholder}
     onChange={onChangeHandler}
+    value={searchField}
   />
 );
 
